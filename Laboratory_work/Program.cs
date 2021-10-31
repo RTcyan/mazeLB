ee26085de2f91c4142bfc1376a58fa47441e2fd9
Maxim <mgudkov@digital-spirit.ru>
2021-10-31 19:03:49 +0400

    Finish labaratory work

---
+++ Laboratory_work/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratory_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
